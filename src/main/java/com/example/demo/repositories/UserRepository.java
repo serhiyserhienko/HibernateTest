6acea64339fbf49287ac11a7a6a6188fa9f3481d
serhiy <serhiy>
2018-10-02 18:28:18 +0300

    enable caching

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo.repositories;
 
 import com.example.demo.models.User;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @Override
+    @Cacheable("users")
+    Optional<User> findById(Integer integer);
 }
